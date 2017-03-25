4831baeac7e9bc294d9fd7095fd1dd50ad9d252a
Nathan Durst <nadurst0303@my.mwsu.edu>
2017-03-25 12:51:49 -0500

    add Java to analyzer

---
+++ SMCS/analyzer/Java/scanner.h
@@ -0,0 +1,80 @@
+//types
+#define INT 1
+#define CHAR 2
+#define FLOAT 3
+#define STRING 4
+#define BOOL 5
+#define CONST 6
+#define FINAL 7
+#define VOID 8
+#define STATIC 9
+
+//type names
+#define tINT 11
+#define tCHAR 12
+#define tFLOAT 13
+#define tSTRING 14
+#define tBOOL 15
+
+//symbols
+#define SEMICOLON 21
+#define COLON 22
+#define LEFT_BRACE 23
+#define RIGHT_BRACE 24
+#define DOUBLE_QUOTE 25
+#define SINGLE_QUOTE 26
+
+//other
+#define FUNCTION 31
+#define VAR_NAME 32
+#define IMPORT 33
+#define PACKAGE 34
+#define IDENTIFIER 35
+#define FUNCTION_CALL 36
+#define RETURN 37
+
+//relational operators
+#define EQ 41
+#define NE 42
+#define GT 43
+#define LT 44
+#define GE 45
+#define LE 46
+
+//arithmetic operators
+#define ADD 51
+#define SUB 52
+#define MUL 53
+#define DIV 54
+#define MOD 55
+#define INC 56
+#define DEC 57
+
+//assignment operators
+#define ASSIGNMENT 61
+
+//control structures
+#define IF 71
+#define ELSE 72
+#define WHILE 73
+#define FOR 74
+#define SWITCH 75
+#define DO 76
+#define TRY 77
+#define CATCH 78
+#define CASE 79
+#define BREAK 80
+#define GOTO 81
+#define CONTINUE 82
+#define THROW 83
+#define FINALLY 84
+
+//data structures
+#define CLASS 91
+#define STRUCT 92
+#define INTERFACE 93
+
+//access modifiers
+#define PRIVATE #101
+#define PUBLIC #102
+#define PROTECTED #103
