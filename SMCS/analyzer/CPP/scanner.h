23ace843c9c673e2e0c75c13b863d1cb9172f3e5
dddong <dddongda@gmail.com>
2017-03-25 12:50:56 -0500

    Add files via upload

---
+++ SMCS/analyzer/CPP/scanner.h
@@ -0,0 +1,69 @@
+//types
+#define INT 1
+#define CHAR 2
+#define FLOAT 3
+#define STRING 4
+#define BOOL 5
+
+//type names
+#define tINT 11
+#define tCHAR 12
+#define tFLOAT 13
+#define tSTRING 14
+#define tBOOL 15
+
+//symbols
+#define SEMICOLON 21
+#define COLON 22
+#define LEFT_BRACE 23
+#define RIGHT_BRACE 24
+#define DOUBLE_QUOTE 25
+#define SINGLE_QUOTE 26
+#define NEWLINE 27
+
+//other
+#define FUNCTION 31
+#define VAR_NAME 32
+#define INCLUDE 33
+#define HEADER 34
+#define IDENTIFIER 35
+#define FUNCTION_CALL 36
+#define RETURN 37
+#define LINE_COMMENT 38
+#define BLOCK_COMMENT 39
+
+//relational operators
+#define EQ 41
+#define NE 42
+#define GT 43
+#define LT 44
+#define GE 45
+#define LE 46
+#define NOT 47
+#define OR 48
+#define AND 49
+
+//arithmetic operators
+#define ADD 51
+#define SUB 52
+#define MUL 53
+#define DIV 54
+#define MOD 55
+#define INC 56
+#define DEC 57
+
+//assignment operators
+#define ASSIGNMENT 61
+#define ADDE       62
+#define SUBE       63
+#define MULE       64
+#define DIVE       65
+#define MODE       66
+
+//bitwise operators
+#define BADD 71
+#define BOR  72
+#define BXOR 73
+#define BNOT 74
+#define BSHL 75
+#define BSHR 76
