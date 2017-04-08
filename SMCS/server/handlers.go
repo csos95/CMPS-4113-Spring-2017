0c0a79493a975ac3896c182a82145a9873b307e9
csos95 <csoscss@gmail.com>
2017-04-08 11:50:22 -0500

    Make metrics checkboxes appear based on available
    Metrics shown on the submit page are now generated from available metrics, not hardcoded.

--- SMCS/server/handlers.go
+++ SMCS/server/handlers.go
@@ -14,6 +14,7 @@ type Page struct {
 	Source     string
 	Analysis   analyzer.Analysis
 	Language   *analyzer.Language
+	Metrics    map[string]analyzer.Metric
 }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, *Server), s *Server) func(http.ResponseWriter, *http.Request) {
@@ -24,23 +25,11 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, *Server), s *Server
 
 func indexHandler(w http.ResponseWriter, r *http.Request, s *Server) {
 	if r.Method == "GET" {
-		page := &Page{Config: s.Config, Extensions: s.Analyzer.Extensions()}
+		page := &Page{Config: s.Config, Extensions: s.Analyzer.Extensions(), Metrics: s.Analyzer.Metrics}
 		s.Template.ExecuteTemplate(w, "index.html", page)
 	}
 }
-//<input type="checkbox" name="loc" value="Lines of Code">Lines of Code<br>
-//<input type="checkbox" name="lod" value="Lines of Documentation">Lines of Documentation<br>
-//<input type="checkbox" name="loc/lod" value="Ratio of LOC to LOD">Ratio of LOC to LOD<br>
-//<input type="checkbox" name="bl" value="Blank Lines">Blank Lines<br>
-//<input type="checkbox" name="tl" value="Total Lines">Total Lines<br>
-//
-//<input type="checkbox" name="nf" value="Number of Functions">Number of Functions<br>
-//<input type="checkbox" name="nfp" value="Number of Function Parameters">Number of Function Parameters<br>
-//<input type="checkbox" name="nc" value="Number of Classes">Number of Classes<br>
-//<input type="checkbox" name="mc" value="Methods per Class">Methods per Class<br>
-//<input type="checkbox" name="lf" value="Lines per Function">Lines per Function<br>
-//
-//<input type="checkbox" name="cc" value="Cyclomatic Complexity">Cyclomatic Complexity<br>
+
 func metricsHandler(w http.ResponseWriter, r *http.Request, s *Server) {
 	if r.Method == "POST" {
 		r.ParseMultipartForm(32 << 20)
