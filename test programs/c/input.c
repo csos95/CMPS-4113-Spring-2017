2bb021dd9421e893551df11a35aa3432f63d4672
csos95 <csoscss@gmail.com>
2017-05-02 10:09:23 -0500

    revert c block_comment regex

---
+++ test programs/c/input.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//this is a line comment
+/*
+this is a block comment
+*/
+int main(void) {
+    /*
+    * this is a block comment
+    */
+    int x = 2;
+    float y = 3.5;
+    float z = x * y;
+    int w = x + x;
+    int u = w - x;
+    printf("z: %d\n", z);
+    return 0;
+}
+/* another block*/
