b84a7838c7d5535cc0336b23401d027a8b330a42
csos95 <csoscss@gmail.com>
2017-05-01 23:54:48 -0500

    Comment main.go, server.go, and modify presentation

--- SMCS/main.go
+++ SMCS/main.go
@@ -9,8 +9,10 @@ import (
 func main() {
 	fmt.Println("Welcome to SMCS.")
 
+	//Create a new server
 	server := server.NewServer("config.json")
 
+	//Run the server
 	err := server.Run()
 	if err != nil {
 		log.Println(err)
--- SMCS/server/server.go
+++ SMCS/server/server.go
@@ -16,35 +16,46 @@ import (
 	"time"
 )
 
+//Config holds the application configuration options
 type Config struct {
 	Domain string `json:"domain"`
 	Port   string `json:"port"`
 	Mode   string `json:"mode"`
 }
 
-func NewConfig(filepath string) (*Config, error) {
+//LoadConfig loads the config from the specified path
+func LoadConfig(filepath string) (*Config, error) {
 	config := &Config{}
 
+	//attempt to read config file
 	file, err := ioutil.ReadFile(filepath)
 	if err != nil {
+		//create a default config if that file doesn't exist
 		return DefaultConfig(filepath)
 	}
 
+	//parse the config into the config struct
 	err = json.Unmarshal(file, &config)
 	if err != nil {
 		return nil, err
 	}
+
 	return config, nil
 }
 
+//DefaultConfig creates, writes, and returns the default config
 func DefaultConfig(filepath string) (*Config, error) {
+	//the default config
 	config := &Config{Domain: "0.0.0.0", Port: "8080", Mode: "server"}
 
+	//marshal it into a json
 	js, _ := json.Marshal(config)
 
+	//get the parent filepath of the config file
 	parts := strings.Split(filepath, "/")
 	path := strings.Join(parts[:len(parts)-1], "/")
 
+	//make sure that filepath exists before trying to put a file there
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err = os.MkdirAll(path, os.ModePerm)
 		if err != nil {
@@ -52,32 +63,41 @@ func DefaultConfig(filepath string) (*Config, error) {
 		}
 	}
 
+	//write the default config
 	err := ioutil.WriteFile(filepath, js, os.ModePerm)
 	if err != nil {
 		return config, err
 	}
+
 	return config, nil
 }
 
+//Handler is a handler function that accepts a server
 type Handler func(http.ResponseWriter, *http.Request, *Server)
 
+//Server contains the application config, webpage templates, and the analyzer
 type Server struct {
 	Config   *Config
 	Template *template.Template
 	Analyzer *analyzer.Analyzer
 }
 
+//NewServer creates a new server with the specified config file
 func NewServer(filepath string) *Server {
-	config, err := NewConfig(filepath)
+	//load the config
+	config, err := LoadConfig(filepath)
 	if err != nil {
 		log.Println(err)
 	}
 
+	//create a new analyzer
 	analyzer := analyzer.NewAnalyzer()
 
+	//create the template with the two webpage templates
 	tmpl := template.New("")
 	files := []string{"index", "metrics"}
 
+	//load them from the bindata_assetfs
 	for _, file := range files {
 		data, err := Asset(fmt.Sprintf("assets/tmpl/%s.html", file))
 		if err != nil {
@@ -91,18 +111,23 @@ func NewServer(filepath string) *Server {
 	return &Server{Config: config, Template: tmpl, Analyzer: analyzer}
 }
 
+//Run executes the webserver
 func (s *Server) Run() error {
+	//create a new mux router
 	router := mux.NewRouter()
 
+	//setup the assets handlers
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(assetFS())))
 	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/assets/img/favicon.ico", http.StatusMovedPermanently)
 	})
 
+	//setup the template handlers
 	router.HandleFunc("/", makeHandler(indexHandler, s)).Methods("GET")
 	router.HandleFunc("/index.html", makeHandler(indexHandler, s)).Methods("GET")
 	router.HandleFunc("/metrics.html", makeHandler(metricsHandler, s)).Methods("POST")
 
+	//if running in standalone, launch the users default browser
 	if s.Config.Mode == "standalone" {
 		go func() {
 			time.Sleep(time.Second * 2)
@@ -110,6 +135,7 @@ func (s *Server) Run() error {
 		}()
 	}
 
+	//create a server using mux router
 	srv := &http.Server{
 		Handler: router,
 		Addr:    s.Config.Domain + ":" + s.Config.Port,
@@ -118,6 +144,7 @@ func (s *Server) Run() error {
 		ReadTimeout:  15 * time.Second,
 	}
 
+	//start the server
 	err := srv.ListenAndServe()
 	if err != nil {
 		return err
